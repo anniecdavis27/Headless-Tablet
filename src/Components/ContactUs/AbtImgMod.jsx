3aa0f51d0794761abb44a0f22e08a789336c84d6
Annie Davis <anniedavis@Annies-MacBook-Pro.local>
2022-04-05 14:15:13 -0400

    mobile contact finished

--- src/Components/ContactUs/AbtImgMod.jsx
+++ src/Components/ContactUs/AbtImgMod.jsx
@@ -5,9 +5,10 @@ function AboutImageModule({ images }) {
 
   return (
     <div className="AboutImageModule">
-      {images.map((i) => {
-        return <div className="image-slot">
-          <img src={i} alt="staff image" />
+      {images.map((i, index) => {
+        console.log(index)
+        return <div className={`image-slot ${index > 3 ? "img-hidden" : ''}`}>
+          <img src={i} alt='staff image' />
         </div>;
       })}
     </div>
